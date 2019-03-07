e543136fec81ca928fa9882d5e51feb2c2d9c08c
austinoboyle <14arob@queensu.ca>
2019-03-06 22:48:02 -0500

    Fork online_photometric_calibration into standard catkin workspace

---
+++ src/online_photometric_calibration/src/StandardIncludes.h
@@ -0,0 +1,44 @@
+//
+//  OpencvIncludes.h
+//  OnlinePhotometricCalibration
+//
+//  Created by Paul on 16.11.17.
+//  Copyright (c) 2017-2018 Paul Bergmann and co-authors. All rights reserved.
+//
+//  See LICENSE.txt
+//
+
+/**
+ * Defines all kinds of standard includes that are used in most of the classes
+ */
+
+#ifndef OnlineCalibration_StandardIncludes_h
+#define OnlineCalibration_StandardIncludes_h
+
+/**
+ * Opencv includes
+ */
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+/**
+ * Standard library includes
+ */
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <vector>
+#include <fstream>
+
+/**
+ * ROS includes
+ */
+// #include <ros/ros.h>
+// #include <image_transport/image_transport.h>
+// #include <cv_bridge/cv_bridge.h>
+
+#endif
