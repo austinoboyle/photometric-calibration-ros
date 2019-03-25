2f86ffee302f0f5969186521463a16256e2168b0
Austin O'Boyle <14arob@queensu.ca>
2019-03-24 23:42:54 -0400

    Only record 16-bit imgs, pcl2, and exposure
    Modify record.sh, we only need these topics to do our analysis.

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rosbag record /SwissRanger/distance/image_raw /SwissRanger/distance/image_raw16 /SwissRanger/intensity/image_raw /SwissRanger/parameter_updates /SwissRanger/parameter_descriptions /SwissRanger/pointcloud2_raw --duration=$1 -O $2
+rosbag record /SwissRanger/intensity/image_raw16 /SwissRanger/distance/image_raw16 /SwissRanger/exposure /SwissRanger/pointcloud2_raw --duration=$1 -O $2
